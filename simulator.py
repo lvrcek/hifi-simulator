d8ef8754ae1ed157efb06b0f4a959e98b35bbf72
Lovro Vrcek <vrcek.lovro@gmail.com>
2022-03-24 22:13:33 +0800

    Remove useless phred scores

--- simulator.py
+++ simulator.py
@@ -67,7 +67,7 @@ def sample_strand(reference, reads_list, length_mean, length_std, step_mean, ste
         else:
             read.description = f'idx={idx}, strand=-, start={len(reference)-position}, end={len(reference)-position-length}'
 
-        read.letter_annotations = {'phred_quality': [50] * len(read)}
+        # read.letter_annotations = {'phred_quality': [50] * len(read)}
         reads_list.append(read)
         step = int(np.random.normal(step_mean, step_std))
         position += step
@@ -79,8 +79,8 @@ def sample_strand(reference, reads_list, length_mean, length_std, step_mean, ste
 def main(args):
     reference_path = os.path.abspath(args.ref)
     out_path = os.path.abspath(args.out)
-    assert re.findall('.*\.([A-Za-z]*)', out_path)[-1] in ('fastq', 'fq'), \
-            "Output path should be in the FASTQ format"
+    assert re.findall('.*\.([A-Za-z]*)', out_path)[-1] in ('fastq', 'fq', 'fasta', 'fa'), \
+            "Output path should be in the FASTA/Q format"
     depth = args.depth
     length_mean = args.length_mean
     length_std = args.length_std if args.length_std is not None else length_mean * 0.075
@@ -97,13 +97,13 @@ def main(args):
     # Sample positive and negative strand
     sample_strand(reference, reads_list, length_mean, length_std, step_mean, step_std, subs, indel, strand='+')
     sample_strand(reference_rc, reads_list, length_mean, length_std, step_mean, step_std, subs, indel, strand='-')
-    SeqIO.write(reads_list, out_path, 'fastq')
+    SeqIO.write(reads_list, out_path, 'fasta')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Takes reference path and path where to store the generated reads.')
     parser.add_argument('--ref', type=str, default='debug/reference.fasta', help='reference path')
-    parser.add_argument('--out', type=str, default='debug/reads.fastq', help='path where to store the reads')
+    parser.add_argument('--out', type=str, default='debug/reads.fasta', help='path where to store the reads')
     parser.add_argument('--length-mean', type=int, default=20000, help='mean length of the simulated reads')
     parser.add_argument('--length-std', type=int, help='standard deviation in length of the simulated reads')
     parser.add_argument('--depth', type=int, default=20, help='sequencing depth to be simulated')
